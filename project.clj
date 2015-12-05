3628afc2c17032f4c1ce6760c96a6195335dec61
Shane Kilkelly <shane@kilkelly.me>
2015-12-05 11:06:50 +0000

    update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject omni "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Simple, global system manager"
+  :url "https://github.com/ShaneKilkelly/clj-omni"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
