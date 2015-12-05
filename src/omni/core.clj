af287748e43a46acb7164fcc23b8c048d9202d57
Shane Kilkelly <shane@kilkelly.me>
2015-12-05 10:51:05 +0000

    docstrings

--- src/omni/core.clj
+++ src/omni/core.clj
@@ -1,25 +1,37 @@
-(ns omni.core)
+(ns omni.core
+  (:require [omni.core :as omni]))
 
 
 (defonce system-map (atom {}))
 
 
-(defn register! [key val]
+(defn register!
+  "Register a value in the system map under a given key.
+  Example: `(omni/register! :db {:connection ...})`"
+  [key val]
   (do
     (swap! system-map assoc key val)
     val))
 
 
-(defn want [key]
+(defn want
+  "Get a value from the system map, identified by the supplied key.
+  Produces nil if the key is not present"
+  [key]
   (key @system-map))
 
 
-(defn want-or-die [key]
+(defn need!
+  "Get a value from the system map, identified by the supplied key.
+  Raises an Exception if the key is not present"
+  [key]
   (let [val (key @system-map)]
     (if (nil? val)
       (throw (Exception. (str "No registered component found for " key)))
       val)))
 
 
-(defn reset! []
+(defn clear!
+  "Empty the system map."
+  []
   (swap! system-map (fn [_] {})))
