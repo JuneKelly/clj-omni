24f8b1d419c79ada2ebbb7b7b221c8cc1f92439a
Shane Kilkelly <shane@kilkelly.me>
2015-12-05 11:03:35 +0000

    tests

--- test/omni/core_test.clj
+++ test/omni/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [omni.core :as omni]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(deftest basics
+
+  (testing "retrieving with no key in system"
+    (do
+      (omni/clear!)
+      (let [db (omni/want :db)]
+        (is (nil? db)))))
+
+  (testing "adding and retrieving"
+    (do
+      (omni/register! :db {:host "localhost" :user "user" :pass "pass"})
+      (let [db (omni/want :db)]
+        (is (not (nil? db)))
+        (is (= "user" (:user db)))))))
